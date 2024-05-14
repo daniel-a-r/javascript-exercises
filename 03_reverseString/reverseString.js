5e56d225d9058ed6345f6fe391f15fb01cdcb138
daniel-a-r <daniel.a-r@outlook.com>
2024-05-13 22:00:45 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    result = str.split('');
+    left = 0;
+    right = str.length - 1;
+    while (left < right) {
+        const temp = result[left];
+        result[left] = result[right];
+        result[right] = temp;
+        left++;
+        right--;
+    }
+    return result.join('');
 };
 
 // Do not edit below this line
