61d44b39f167a723ab902a70e7f58dce44927684
daniel-a-r <daniel.a-r@outlook.com>
2024-05-13 22:29:36 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args) {
+        let startIndx = 0;
+        while (true) {
+            const indx = arr.indexOf(arg, startIndx);
+            if (indx === -1) break;
+            arr.splice(indx, 1);
+            startIndx = indx;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
