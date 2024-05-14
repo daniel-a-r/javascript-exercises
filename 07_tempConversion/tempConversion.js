d31c770d8efea30b8bc29e1b309c0c8901b2787c
daniel-a-r <daniel.a-r@outlook.com>
2024-05-13 23:19:28 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  if (f === 32) return 0;
+
+  return Number(((f - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(c) {
+  if (c === 0) return 32;
+
+  return Number((c * (9 / 5) + 32).toFixed(1));
+}; 
 
 // Do not edit below this line
 module.exports = {
