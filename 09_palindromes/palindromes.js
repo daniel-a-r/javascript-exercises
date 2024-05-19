137e210dd651a0fcb9e23266b21ac3444e985520
daniel-a-r <daniel.a-r@outlook.com>
2024-05-19 02:54:12 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let formattedStr = string
+                        .toLowerCase()
+                        .replace(/[^a-z0-9]/gi, '');
+    let reversedStr = formattedStr
+                        .split('')
+                        .reverse()
+                        .join('');
+    return formattedStr === reversedStr;
 };
 
 // Do not edit below this line
