b884fdf3e4d4cfbd92065ebaca5a0613bcc4b8ff
daniel-a-r <daniel.a-r@outlook.com>
2024-05-19 04:02:38 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const getAge = person => {
+    let age;
+    if ('yearOfDeath' in person) {
+        age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+        const currentYear = new Date().getFullYear();
+        age = currentYear - person.yearOfBirth;
+    }
+    return age;
+}
 
+const findTheOldest = function(people) {
+    let oldest = people.reduce((oldestPerson, currentPerson) => {
+        return getAge(oldestPerson) < getAge(currentPerson) ? currentPerson : oldestPerson;
+    });
+    return oldest;
 };
 
 // Do not edit below this line
