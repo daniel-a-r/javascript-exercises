96fb61a5b14405baaacb045dea5b4bce807cfbb2
daniel-a-r <daniel.a-r@outlook.com>
2024-05-19 03:12:05 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+    if (typeof val === 'string') val = Number(val);
+    if (val < 0) return 'OOPS'; 
+    if (val < 2) return val;
+    let prev = 0;
+    let current = 1;
 
+    for (let i = 1; i < val; i++) {
+        let temp = prev + current;
+        prev = current;
+        current = temp
+    }
+
+    return current;
 };
 
 // Do not edit below this line
